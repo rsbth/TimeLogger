ee8c6411ef53fd2c447eda9c4227800eaf29d053
mprtcz <azet89@gmail.com>
2017-01-17 14:40:58 +0100

    init android commit

---
+++ time-logger-android/app/src/main/java/timelogger/mprtcz/com/timelogger/MainActivity.java
@@ -0,0 +1,13 @@
+package timelogger.mprtcz.com.timelogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
