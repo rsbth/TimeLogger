7d41d7df28fbc5a71904d81f46a8d727ea09ec27
mprtcz <azet89@gmail.com>
2017-02-02 13:32:41 +0100

    Minor refactoring;

--- time-logger-desktop/src/main/java/com/mprtcz/timeloggerdesktop/backend/utilities/webutils/RetrofitUtil.java
+++ time-logger-desktop/src/main/java/com/mprtcz/timeloggerdesktop/backend/utilities/webutils/RetrofitUtil.java
@@ -13,7 +13,7 @@ import java.util.Date;
  * Created by mprtcz on 2017-01-26.
  */
 public class RetrofitUtil {
-    private static final String BASE_URL = "http://localhost:8080";
+    private static final String BASE_URL = "http://192.168.0.11:8080";
     private Retrofit retrofit;
     private static RetrofitUtil retrofitUtil;
 
--- time-logger-server/src/main/java/com/mprtcz/timeloggerserver/TimeLoggerApplication.java
+++ time-logger-server/src/main/java/com/mprtcz/timeloggerserver/TimeLoggerApplication.java
@@ -3,12 +3,13 @@ package com.mprtcz.timeloggerserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 @EntityScan(basePackageClasses = { TimeLoggerApplication.class, Jsr310JpaConverters.class })
-@SpringBootApplication
+@SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class})
 public class TimeLoggerApplication {
 
 	public static void main(String[] args) {
