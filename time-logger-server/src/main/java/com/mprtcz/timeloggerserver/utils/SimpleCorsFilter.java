6655433511e0a17f719eaec878f2b82958c9fccd
mprtcz <azet89@gmail.com>
2017-02-15 18:52:07 +0100

    Added CORS filter to the server;

---
+++ time-logger-server/src/main/java/com/mprtcz/timeloggerserver/utils/SimpleCorsFilter.java
@@ -0,0 +1,32 @@
+package com.mprtcz.timeloggerserver.utils;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by mprtcz on 2017-02-15.
+ */
+@Component
+public class SimpleCorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Expose-Headers", "Location");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {}
+}
